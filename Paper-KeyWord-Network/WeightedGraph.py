afec3c43a808a99dca43144b38e13515ff57fca8
Am-Coder <mishraaman2210@gmail.com>
2021-03-01 19:53:49 +0530

    Added BFS for key searchtill level k

--- Paper-KeyWord-Network/WeightedGraph.py
+++ Paper-KeyWord-Network/WeightedGraph.py
@@ -30,14 +30,43 @@ class Graph:
             for node in self.graph[i]:
                 print(str(node.dest) + "(Weight = " + str(node.wt) + ")" + " ")
             print("\n")
-
+    
+    def BFS(self, s, max_levels):
+        visited = [False] * (self.V + 1)
+ 
+        queue = []
+ 
+        queue.append(s)
+        visited[s] = True
+        level = 0
+        result = {}
+        while queue:
+            aux = []
+            result[level] = []
+            
+            while queue:
+                s = queue.pop(0)
+                visited[s] = True
+                result[level].append(s)
+                for node in self.graph[s]:
+                    if visited[node.dest] == False:
+                        aux.append(node.dest)
+                        visited[node.dest] = True
+            level += 1
+            if level > max_levels:
+                break
+            for node in aux:
+                queue.append(node)
+            
+        return result
+            
+                    
 # Create a graph SAMPLE use 
 g = Graph(4) 
 g.addEdge(0, 1, 2) 
 g.addEdge(0, 2, 2) 
 g.addEdge(1, 2, 1) 
 g.addEdge(1, 3, 1) 
-g.addEdge(2, 0, 1) 
 g.addEdge(2, 3, 2) 
-g.addEdge(3, 3, 2)
-g.printGraph()
+# g.printGraph()
+print(g.BFS(3,3))
